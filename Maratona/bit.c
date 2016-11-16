228a9c8faeca367d23fe147bf9f0190d3faaa21a
José Joaquim <Jos Joaquim>
2016-11-16 11:33:18 -0300

    remove files

--- Maratona/bit.c
+++ Maratona/bit.c
@@ -2,10 +2,12 @@
 
 int main(int argc, char **argv)
 {
-  int x = 0;
-  //x |= (1 << 2);
-  x |= (1 << 3);
-  printf("%d\n", x);// (x >> 3) & 1);
-  return 0;
+	int x = 0;
+	//x |= (1 << 2);
+	x |= (1 << 3);
+
+  
+	printf("%d\n", x);// (x >> 3) & 1);
+	return 0;
 }
 
